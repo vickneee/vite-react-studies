184ef12c54f7799af59b31cd2a21cff77bfff88e
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-03-21 15:32:49 +0200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddBook from './AddBook';
+
+import './App.css';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [books, setBooks] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const columnDefs = [
+        {field: 'title', sortable: true, filter: true },
+        {field: 'author', sortable: true, filter: true },
+        {field: 'year', sortable: true, filter: true },
+        {field: 'isbn', sortable: true, filter: true },
+        {field: 'price', sortable: true, filter: true },
+        {
+            headerName: '',
+            field: 'id',
+            width: 70,
+            cellRenderer: params =>
+                <IconButton onClick={() => deleteBook(params.value)} size="small" color="error">
+                    <DeleteIcon />
+                </IconButton>
+        }
+    ];
+
+    useEffect(() => {
+        fetchItems();
+    }, [])
+
+    const fetchItems = () => {
+        fetch('https://bookstore-d6fd1-default-rtdb.europe-west1.firebasedatabase.app/books/.json')
+            .then(response => response.json())
+            .then(data => addKeys(data))
+            .catch(err => console.error(err))
+    }
+
+    // Add keys to the book objects
+    const addKeys = (data) => {
+        const keys = Object.keys(data);
+        const valueKeys = Object.values(data).map((item, index) =>
+            Object.defineProperty(item, 'id', {value: keys[index]}));
+        setBooks(valueKeys);
+    }
+
+    const addBook = (newBook) => {
+        fetch('https://bookstore-d6fd1-default-rtdb.europe-west1.firebasedatabase.app/books/.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(newBook)
+            })
+            .then(response => fetchItems())
+            .catch(err => console.error(err))
+    }
+
+    const deleteBook = (id) => {
+        fetch(`https://bookstore-d6fd1-default-rtdb.europe-west1.firebasedatabase.app/books/${id}.json`,
+            {
+                method: 'DELETE',
+            })
+            .then(response => fetchItems())
+            .catch(err => console.error(err))
+    }
+
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h5" noWrap>
+                        Bookstore
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <AddBook addBook={addBook} />
+            <div className="ag-theme-material" style={ { height: 400, width: 1075, margin: 'auto' } }>
+                <AgGridReact
+                    rowData={books}
+                    columnDefs={columnDefs}
+                />
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
